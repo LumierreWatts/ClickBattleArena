b74e51de8ae7a461a660d6e6a4ed35260cafbeac
LumierreWatts <leonilapaya8@gmail.com>
2025-07-29 18:18:19 +0800

    comment remember user

--- src/provider/ReactTogetherWrapper.jsx
+++ src/provider/ReactTogetherWrapper.jsx
@@ -11,7 +11,7 @@ export default function ReactTogetherWrapper({ children }) {
         name: process.env.NEXT_PUBLIC_NAME,
         password: process.env.NEXT_PUBLIC_PASSWORD,
       }}
-      rememberUsers={true}
+      // rememberUsers={true}
     >
       {children}
     </ReactTogether>
